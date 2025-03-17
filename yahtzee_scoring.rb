7ab5ff1154bd77ce7eca51491cc2196283a605f0
Mark Yen <77414433+markcyen@users.noreply.github.com>
2025-03-16 23:45:19 -0600

    implement returns as hash instead of arrays

--- yahtzee_scoring.rb
+++ yahtzee_scoring.rb
@@ -2,47 +2,46 @@ class YahtzeeScoring
   @roll = []
   @tally_roll = {}
 
+  # best_score returns an array of best categories with the highest scores from both sections
   def self.best_score(roll = [])
     @roll = roll
     @tally_roll = @roll.tally
 
     raise ArgumentError, "Error: The number of die being rolled should be five." if @roll.length != 5
 
-    best = [score_upper_section, score_lower_section].max_by { |roll| roll[:score] }
-
-    { category: best[:category], score: best[:score] }
+    combined_sections = score_upper_section.merge(score_lower_section)
+    highest_score = combined_sections.values.max
+    combined_sections.select { |category, score| score == highest_score}
   end
 
   # score_upper_section determines the best_category and best_score for the upper section
+  # and returns a hash of of best categories that have the highest score
   def self.score_upper_section
     num_to_category = { 1 => :ones, 2 => :twos, 3 => :threes, 4 => :fours, 5 => :fives, 6 => :sixes }
 
-    # This method gets the best score first by the frequency
-    # If the score is tied, then this method gets the number and frequency by the highest dice number
-    number, frequency = @tally_roll.max_by { |num, freq| [num * freq, num] }
-    best_score = number * frequency
-    best_category = num_to_category[number]
+    best_scores = {}
+    highest_score = @tally_roll.map { |num, freq| num * freq }.max
+    best_scores = @tally_roll.select { |num, freq| num * freq == highest_score }
  
-    { category: best_category, score: best_score }
+    best_scores.map { |num, freq| [num_to_category[num], num * freq] }.to_h
   end
 
   # score_lower_section determines the best_category and best_score for the lower section
+  # and returns a hash of of best categories that have the highest score
   def self.score_lower_section
     sum_roll = @roll.sum
 
-    categories = []
-    categories << { category: :yahtzee, score: 50 } if is_yahtzee?()
-    categories << { category: :large_straight, score: 40 } if is_large_straight?()
-    categories << { category: :small_straight, score: 30 } if is_small_straight?()
-    categories << { category: :full_house, score: 25 } if is_full_house?()
-    categories << { category: :four_of_a_kind, score: sum_roll } if is_four_of_a_kind?()
-    categories << { category: :three_of_a_kind, score: sum_roll } if is_three_of_a_kind?()
-    categories << { category: :chance, score: sum_roll }
+    categories = {}
+    categories[:yahtzee] = 50 if is_yahtzee?()
+    categories[:large_straight] = 40 if is_large_straight?()
+    categories[:small_straight] = 30 if is_small_straight?()
+    categories[:full_house] = 25 if is_full_house?()
+    categories[:four_of_a_kind] = sum_roll if is_four_of_a_kind?()
+    categories[:three_of_a_kind] = sum_roll if is_three_of_a_kind?()
+    categories[:chance] = sum_roll
 
-    # If there are two scores that are equal, this function extracts the first one in the array
-    best = categories.max_by { |roll| roll[:score] }
-
-    { category: best[:category], score: best[:score] }
+    highest_score = categories.values.max
+    categories.select { |category, score| score == highest_score}
   end
 
   # Checks for lower section
